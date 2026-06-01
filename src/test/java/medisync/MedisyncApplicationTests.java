de0f14af302ddb3b14ae3270f5db7ea37cd3f05f
mason <mason@mason.local>
2026-06-01 19:55:10 +0900

    feat : 프로젝트 초기 설정 및 환경 구성
    - 패키지명 spring_boot_starter_kit -> medisync 변경
    - 도메인 패키지 구조 설정
    - MySQL 데이터소스 설정
    - docker-compose 환경별 분리 (공통 / prod)
    - .env 기반 환경변수 관리 설정
    - bootRun 시 .env 파일 자동 로드 설정

--- src/test/java/medisync/MedisyncApplicationTests.java
+++ src/test/java/medisync/MedisyncApplicationTests.java
@@ -6,7 +6,7 @@ import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
-class SpringBootStarterKitApplicationTests {
+class MedisyncApplicationTests {
 
 	@Test
 	void contextLoads() {
